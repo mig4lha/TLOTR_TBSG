c9d9ad0654d648946b70737d07dee01be88bfedc
estagiosidev <estagiosidev@bestgest.pt>
2023-11-15 11:10:56 +0000

    Test of grid mechanics

--- grid_mechanics.c
+++ grid_mechanics.c
@@ -28,13 +28,54 @@ void inititializeGrid() {
     printf("Cols: %d\n", grid.cols);
     printf("\n");
 
-    int counter = 1;
-
-    // Accessing matrix elements
-    for (int i = 0; i < grid.rows; i++) {
-        for (int j = 0; j < grid.cols; j++) {
-            printf("%4d ", counter);
-            counter++;
+    // Accessing Matrix elements
+    for (int lineCounter = 0; lineCounter < grid.rows; lineCounter++) {
+        for (int columnCounter = 0; columnCounter < grid.cols; columnCounter++) {
+            if (lineCounter == 0 && columnCounter == 25) {
+                printf("%c", 191);
+            }
+            else if (lineCounter == 16 && columnCounter == 0) {
+                printf("%4c", 192);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
+            else if (lineCounter == 16 && columnCounter == 25) {
+                printf("%c", 217);
+            }
+            else if (lineCounter == 0 && columnCounter == 0) {
+                printf("%4c", 218);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
+            else if (lineCounter == 0) {
+                printf("%c", 194);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
+            else if (lineCounter == 16) {
+                printf("%c", 193);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
+            else if (columnCounter == 25) {
+                printf("%c", 180);
+            }
+            else if (columnCounter == 0) {
+                printf("%4c", 195);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
+            else {
+                printf("%c", 197);
+                printf("%c", 196);
+                printf("%c", 196);
+                printf("%c", 196);
+            }
         }
         printf("\n");
     }
